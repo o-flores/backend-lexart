f1ff852cc8c2e43e6d1e5308ba4a64a9d430926d
Orlando Flores <orlando_flores@id.uff.br>
2021-11-02 16:47:09 -0300

    feat: adiciona descrição e trata possiveis erros

--- models/mercadoLivre/index.js
+++ models/mercadoLivre/index.js
@@ -5,12 +5,31 @@ const DB_COLLECTION = 'Lexartlabs';
 
 async function fetchProductsByCategoryAndQuery(categoryId, query) {
   try {
-    const { data } = await axios.get(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+    const { data } = await axios.get(`http://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
     return data.results;
   } catch (error) {
     return error;
   }
 }
+
+const formatInfo = async (productsList) => {
+  const productsInfo = productsList.map(async (product) => {
+    try {
+      const { data } = await axios.get(`https://api.mercadolibre.com/products/${product.catalog_product_id}`);
+      return {
+        id: product.id,
+        title: product.title,
+        price: product.price,
+        thumbnail: product.thumbnail,
+        link: product.permalink,
+        description: data.short_description.content,
+      };
+    } catch (error) {
+      return error;
+    }
+  });
+  return Promise.all(productsInfo);
+};
 
 const verifiesIfSearchAlreadyExists = async (category, query, webSite) => {
   const db = await getConnection();
@@ -30,26 +49,24 @@ const getProducts = async (data) => {
   } = data;
   const db = await getConnection();
 
-  const productsList = await verifiesIfSearchAlreadyExists(category, search, web);
-  if (productsList) return productsList.results;
+  try {
+    const productsList = await verifiesIfSearchAlreadyExists(category, search, web);
+    if (productsList) return productsList.results;
 
-  const AllproductsInfo = await fetchProductsByCategoryAndQuery(categoryId, search);
-  const productsInfo = AllproductsInfo.map((product) => ({
-    title: product.title,
-    price: product.price,
-    thumbnail: product.thumbnail,
-    link: product.permalink,
-  }));
+    const allproductsInfo = await fetchProductsByCategoryAndQuery(categoryId, search);
+    const productsInfo = await formatInfo(allproductsInfo);
+    const dbInfo = {
+      category,
+      search,
+      web,
+      results: productsInfo,
+    };
 
-  const dbInfo = {
-    category,
-    search,
-    web,
-    results: productsInfo,
-  };
-
-  await db.collection(DB_COLLECTION).insertOne(dbInfo);
-  return productsInfo;
+    await db.collection(DB_COLLECTION).insertOne(dbInfo);
+    return productsInfo;
+  } catch (error) {
+    return error;
+  }
 };
 
 module.exports = {
