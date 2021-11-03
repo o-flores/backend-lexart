3cd075d52a086ded2b0e24d05ef9d0ecdeda94e1
Orlando Flores <orlando_flores@id.uff.br>
2021-11-02 22:54:50 -0300

    feat: adiciona a rota para buscar infos no site do buscape

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const app = require('express')();
 
 const port = 3000;
 const bodyParser = require('body-parser');
-const mercadoController = require('./controllers/mercadoLivre');
+const mercadoLivreController = require('./controllers/mercadoLivre');
+const buscapeController = require('./controllers/buscape');
 
 app.use(bodyParser.json());
 
-app.get('/mercadolivre/:id', mercadoController.getProducts);
+app.get('/mercadolivre/:id', mercadoLivreController.getProducts);
+app.get('/buscape', buscapeController.getProducts);
 
 app.listen(port);
