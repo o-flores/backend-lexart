57a67dde6602335c033215786c461b315ba3b77b
Orlando Flores <orlando_flores@id.uff.br>
2021-11-02 22:51:23 -0300

    feat: corrige função formatInfo, muda função verifiesIfSearchAlreadyExists de arquivo e add tratamento de erros em getProducts

--- models/mercadoLivre/index.js
+++ models/mercadoLivre/index.js
@@ -1,11 +1,12 @@
 const axios = require('axios').default;
 const { getConnection } = require('../connection');
+const { verifiesIfSearchAlreadyExists } = require('../helper');
 
 const DB_COLLECTION = 'Lexartlabs';
 
 async function fetchProductsByCategoryAndQuery(categoryId, query) {
   try {
-    const { data } = await axios.get(`http://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+    const { data } = await axios.get(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
     return data.results;
   } catch (error) {
     return error;
@@ -16,13 +17,14 @@ const formatInfo = async (productsList) => {
   const productsInfo = productsList.map(async (product) => {
     try {
       const { data } = await axios.get(`https://api.mercadolibre.com/products/${product.catalog_product_id}`);
+      const description = data.short_description.content || '';
       return {
         id: product.id,
         title: product.title,
         price: product.price,
         thumbnail: product.thumbnail,
         link: product.permalink,
-        description: data.short_description.content,
+        description,
       };
     } catch (error) {
       return error;
@@ -31,42 +33,29 @@ const formatInfo = async (productsList) => {
   return Promise.all(productsInfo);
 };
 
-const verifiesIfSearchAlreadyExists = async (category, query, webSite) => {
-  const db = await getConnection();
-  const product = await db.collection(DB_COLLECTION).findOne({
-    $and: [
-      { category: { $eq: category } },
-      { search: { $eq: query } },
-      { web: { $eq: webSite } },
-    ],
-  });
-  return product;
-};
-
 const getProducts = async (data) => {
   const {
     category, search, web, categoryId,
   } = data;
   const db = await getConnection();
 
-  try {
-    const productsList = await verifiesIfSearchAlreadyExists(category, search, web);
-    if (productsList) return productsList.results;
+  const productsList = await verifiesIfSearchAlreadyExists(category, search, web);
+  if (productsList) return productsList.results;
 
-    const allproductsInfo = await fetchProductsByCategoryAndQuery(categoryId, search);
-    const productsInfo = await formatInfo(allproductsInfo);
-    const dbInfo = {
-      category,
-      search,
-      web,
-      results: productsInfo,
-    };
+  const allproductsInfo = await fetchProductsByCategoryAndQuery(categoryId, search);
+  if (allproductsInfo.message) return allproductsInfo;
 
-    await db.collection(DB_COLLECTION).insertOne(dbInfo);
-    return productsInfo;
-  } catch (error) {
-    return error;
-  }
+  const productsInfo = await formatInfo(allproductsInfo);
+
+  const dbInfo = {
+    category,
+    search,
+    web,
+    results: productsInfo,
+  };
+
+  await db.collection(DB_COLLECTION).insertOne(dbInfo);
+  return productsInfo;
 };
 
 module.exports = {
