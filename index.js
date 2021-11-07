3099f20f4e8f50163d0bd700a44bdf1b9ca3e99b
Orlando Flores <orlando_flores@id.uff.br>
2021-11-06 22:36:11 -0300

    feat: adiciona cors à aplicação

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const app = require('express')();
+const cors = require('cors');
 
-const port = 3000;
+const port = 3001;
 const bodyParser = require('body-parser');
 const mercadoLivreController = require('./controllers/mercadoLivre');
 const buscapeController = require('./controllers/buscape');
 
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+}));
 app.use(bodyParser.json());
 
-app.get('/mercadolivre/:id', mercadoLivreController.getProducts);
-app.get('/buscape', buscapeController.getProducts);
+app.post('/mercadolivre/:id', mercadoLivreController.getProducts);
+app.post('/buscape', buscapeController.getProducts);
 
 app.listen(port);
